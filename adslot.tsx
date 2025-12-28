6daea9dba629dddce0ff87ee0f587199da58d2cb
Savemate <tedfrans28@gmail.com>
2025-12-28 19:56:43 +0200

    Create adslot.tsx

---
+++ adslot.tsx
@@ -0,0 +1,7 @@
+export default function AdSlot() {
+  return (
+    <div className="my-8 p-4 bg-gray-100 text-center text-sm text-gray-500">
+      Sponsored content / Advertisement
+    </div>
+  );
+}
