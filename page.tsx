c9132eea42344ca96e567ef8c5b9d23da8f6e971
Savemate <tedfrans28@gmail.com>
2025-12-28 19:57:41 +0200

    Create page.tsx

---
+++ page.tsx
@@ -0,0 +1,11 @@
+export default function Disclaimer() {
+  return (
+    <section>
+      <h1 className="text-3xl font-bold mb-4">Legal Disclaimer</h1>
+      <p>
+        Information provided is for educational purposes only and does not
+        constitute legal advice.
+      </p>
+    </section>
+  );
+}
