45f00f670b4227738faca0e7072adcdafe42dfda
Savemate <tedfrans28@gmail.com>
2025-12-28 19:51:06 +0200

    Create sitemap.ts

---
+++ sitemap.ts
@@ -0,0 +1,9 @@
+export default function sitemap() {
+  return [
+    { url: "https://9to5university.com", priority: 1 },
+    { url: "https://9to5university.com/rights" },
+    { url: "https://9to5university.com/ccma" },
+    { url: "https://9to5university.com/resume" },
+    { url: "https://9to5university.com/privacy" }
+  ];
+}
