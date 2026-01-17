9700118d19d1e9c6c433f7f645eb56381440afa7
Savemate <tedfrans28@gmail.com>
2026-01-18 01:31:06 +0200

    Delete sitemap.ts

--- sitemap.ts
+++
@@ -1,9 +0,0 @@
-export default function sitemap() {
-  return [
-    { url: "https://9to5university.com", priority: 1 },
-    { url: "https://9to5university.com/rights" },
-    { url: "https://9to5university.com/ccma" },
-    { url: "https://9to5university.com/resume" },
-    { url: "https://9to5university.com/privacy" }
-  ];
-}
